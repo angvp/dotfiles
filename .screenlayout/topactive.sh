93b96cbae003f0d1057da12816653cb7d8cc163a
Ángel Velásquez <angvp@archlinux.org>
2020-01-04 11:42:42 -0500

    Update screenlayout linux

--- .screenlayout/topactive.sh
+++ .screenlayout/topactive.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 2048x1152 --pos 144x2160 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3-1 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output DP-3-3 --off --output DP-3-2 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr --output eDP1 --mode 2560x1440 --pos 0x1440 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --primary --mode 2560x1440 --pos 2583x0 --rotate normal --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
