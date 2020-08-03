a7a85c0c8618196b715ab35716fa61fd348652b7
Ángel Velásquez <angvp@archlinux.org>
2020-08-03 07:16:04 -0400

    Update alacritty, tmux, and screenlayouts
    - Current desktop (3 monitors, 2 vertical)
    - tmux (removing option that was binded to split panes)
    - Update alacritty config (to be compatible with alacritty 0.5.0)

--- .screenlayout/current-desktop.sh
+++ .screenlayout/current-desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output DP3-1 --off --output DP3-2 --off --output DP3-3 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI1 --primary --mode 2560x1440 --pos 1080x167 --rotate normal --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output DP3-1 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DP3-2 --off --output DP3-3 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI1 --mode 2560x1440 --pos 3000x0 --rotate left --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
