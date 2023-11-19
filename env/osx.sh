b4e5de88ebda3d387cccbf98aa9c6480309f7ca2
Ángel Velásquez <me@angvp.com>
2023-11-19 15:46:10 -0700

    Remove some unused env vars

--- env/osx.sh
+++ env/osx.sh
@@ -1,12 +1,3 @@
-VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
-PATH=$PATH:~/Library/Android/sdk/platform-tools
-
-if [ -f /usr/local/bin/virtualenvwrapper.sh ] ; then
-    source /usr/local/bin/virtualenvwrapper.sh
-elif [ -f /usr/bin/virtualenvwrapper.sh ] ; then
-    source /usr/bin/virtualenvwrapper.sh
-fi
-
 # Start keychain
 if [ -f /usr/bin/keychain ] && [ "x$ssh_key" != "x" ]; then
     /usr/bin/keychain -q $ssh_key
@@ -18,12 +9,3 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 
 test -e "${HOME}/env/osxpriv.sh" && source "${HOME}/env/osxpriv.sh"
 export HOMEBREW_GITHUB_API_TOKEN
-
-export ANDROID_HOME="/Users/angvp/Library/Android/sdk"
-export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
-export PATH="/usr/local/opt/php@7.1/bin:$PATH"
-
-# custom aliases
-alias nukemongo="mongo --quiet --eval 'db.getMongo().getDBNames().forEach(function(i){db.getSiblingDB(i).dropDatabase()})'"
-alias launch-redis="redis-server /usr/local/etc/redis.conf"
-alias alacritty="open -n /Applications/Alacritty.app"
